a4951a7f46d39f6d6d21ffd7f5295cceb8221ee9
Patrick Streck <stpt@tgw-group.com>
2026-03-20 11:17:40 +0100

    Added Fibonacci Tools

--- src/App.svelte
+++ src/App.svelte
@@ -143,6 +143,23 @@ onMount(() => {
     }
     chart.update()
 
+    // activate fibo tool script
+    for (var p = 0; p < 2; p++) {
+        chart.data.panes[p].overlays.push({
+            name: 'FiboTool',
+            type: 'FiboTool',
+            data: [],
+            dataExt: {
+                lines: [{}]
+            }, // Here we place non-timeseries data
+            props: {},
+            settings: {
+                zIndex: 1
+            }
+        })
+    }
+    chart.update()
+
     // for (var p = 0; p < 2; p++) {
     //     chart.data.panes[p].overlays.push({
     //         name: 'LineTool',
@@ -334,6 +351,11 @@ function toggleTheme() {
                 </svg>
             </button>
             <button class="btnTool" id="idBtnToolBox">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M384 80c8.8 0 16 7.2 16 16l0 320c0 8.8-7.2 16-16 16L64 432c-8.8 0-16-7.2-16-16L48 96c0-8.8 7.2-16 16-16l320 0zM64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32z"/>
+                </svg>
+            </button>
+            <button class="btnTool" id="idBtnToolFibo">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                     <path d="M384 80c8.8 0 16 7.2 16 16l0 320c0 8.8-7.2 16-16 16L64 432c-8.8 0-16-7.2-16-16L48 96c0-8.8 7.2-16 16-16l320 0zM64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32z"/>
                 </svg>
